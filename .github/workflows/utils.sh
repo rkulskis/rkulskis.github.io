c6b757cb2519076262884ad55b4b926d22ed1988
Ross <rkulskis@bu.edu>
2025-12-15 21:10:46 -0500

    fix? typography

--- .github/workflows/utils.sh
+++ .github/workflows/utils.sh
@@ -62,11 +62,11 @@ generate_header() {
         css_path="${css_path}typography.css"
     fi
     
-    echo "#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"$css_path\" />"
     echo "#+HTML_HEAD: <link rel=\"stylesheet\""
     echo "#+HTML_HEAD:       href=\"https://fonts.googleapis.com/css2?family=Ubuntu+Sans+Mono:wght@400;700&display=swap\">"
     echo "#+HTML_HEAD: <link rel=\"stylesheet\""
     echo "#+HTML_HEAD: href=\"https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap\">"
+    echo "#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"$css_path\" />"
     
     echo "$header"
     echo "#+OPTIONS: toc:nil num:nil"
